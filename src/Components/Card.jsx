71f77aacd1f0c7f3e8657198c08b3d8fe5524808
Arthur Palacio <168678480+Artpbm@users.noreply.github.com>
2025-04-28 01:58:39 -0300

    Add files via upload

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,15 +3,32 @@ import style from './Card.module.css'
 export const Card = ({name, image, ki, maxKi, race, gender, description, affiliation}) => {
     return(
     <>
-    <h5 className={style.tittle}>{name}</h5>
-    <img src={image} alt={name} className={style.imgstyle} />
+    <div className={style.cardsEstilo}>
+          <div className={style.imgCardApi}>
+         <img src={image} alt={name} className={style.imgCard}/>
+         </div >
+         <div className={style.infoApi}>
+         <h1 className={style.title}>{name}</h1>
 
-    <p>Raça: {race}</p>
-    <p>Genêro: {gender}</p>
-    <p>Ki: {ki}</p>
-    <p>Ki máximo: {maxKi}</p>
-    <p>Afiliação: {affiliation}</p>
-    <p>Descrição {description}</p>
+         <h2 className={style.titleDesc}>ki</h2>
+         <p className={style.desc}> {ki}</p>
+
+         <h2 className={style.titleDesc}>Ki Maximo</h2>
+         <p className={style.desc}>{maxKi}</p>
+
+         <h2 className={style.titleDesc}>Raça</h2>
+         <p className={style.desc}>{race}</p>
+
+         <h2 className={style.titleDesc}>Genêro</h2>
+         <p className={style.desc}>{gender}</p>
+
+
+         <h2 className={style.titleDesc}>Afiliação</h2>
+         <p className={style.desc}>{affiliation}</p>
+         </div>
+         
+          </div>
+         
     </>
     )
 }
--- src/Dragon.jsx
+++ src/Dragon.jsx
@@ -2,16 +2,17 @@ import { api } from './api/api'
 import style from './Dragon.module.css'
 import { useState, useEffect } from 'react'
 import { Card } from './Components/Card'
+import { Menu } from './Components/menu'
 
 export default function Dragon(){
     const [data, setData] = useState([])
-    const [page, setPage] = useState("1")
-    
+    const [page, setPage] = useState("")
+    const [showInfo, setShowInfo] = useState(false);
     const [erro, setErro] = useState(false)
 
     useEffect(() => {
 
-        api.get(`/characters?page=1`).then((res) => {
+        api.get(`/characters?page=${page}`).then((res) => {
             setData(res.data.items)
         }).catch((error) => {
             console.log(error)
@@ -22,9 +23,11 @@ export default function Dragon(){
     console.log(data)
 
     return(
+        <>
+        <Menu op1='Voltar'/>
         <section className={style.wrapPage}>
-            <h1 className={style.titlename}>Dragon Ball super</h1>
-            <input type="text" placeholder='digite uma página (1/5)' value={page} onChange={(e) => setPage(e.target.value)} />
+            <input  className={style.wrapInput} type="text" placeholder='digite uma página (1/6)' value={page} onChange={(e) => setPage(e.target.value)} />
+            <br />
             {erro && <p>Página não encontrada</p>}
 
             <div className={style.wrapCard}>
@@ -37,5 +40,6 @@ export default function Dragon(){
                 })}
             </div>
         </section>
+        </>
     )
 }
